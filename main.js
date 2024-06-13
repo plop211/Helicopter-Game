d2bd95f1e1866a6890a63721d10298574e68b22c
plop211 <160066758+plop211@users.noreply.github.com>
2024-06-13 13:13:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// Helicopter Game Start
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables (Once)
+let heliImg = document.createElement("img");
+heliImg.src = "img/heliBlueTransparent.png";
+
+let explosion = document.createElement("audio");
+explosion.src = "sound/explosion.wav";
+
+let propeller = document.createElement("audio");
+propeller.src = "sound/propeller.wav";
+
+let mouseIsPressed = false;
+
+// Global variables (Reset)
+let state;
+let heli;
+let wall1, wall2, wall3;
+reset();
+
+// Draw Function
+window.addEventListener("load", draw);
+
+function draw() {
+  if (state === "start") {
+    drawStart();
+  } else if (state === "gameon") {
+    runGame();
+  } else if (state === "gameover") {
+    drawGameOver();
+  }
+
+  // Request Animation Frame
+  requestAnimationFrame(draw);
+}
+
+// Event stuff
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+
+function mousedownHandler() {
+  mouseIsPressed = true;
+
+  // play propeller sound
+  propeller.setCurrentTime = 0;
+  propeller.play();
+
+  // start game on mouse
+  if (state === "start") {
+    state = "gameon";
+  }
+}
+function mouseupHandler() {
+  mouseIsPressed = false;
+  propeller.pause();
+}
